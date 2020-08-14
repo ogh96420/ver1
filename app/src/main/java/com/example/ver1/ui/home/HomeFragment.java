896536a260d59fd97d947a46bc269c4837bcdc67
hyunker <slp51074@naver.com>
2020-08-15 00:33:59 +0900

    광복절특사

--- app/src/main/java/com/example/ver1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ver1/ui/home/HomeFragment.java
@@ -6,15 +6,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.ver1.R;
 import com.example.ver1.ui.home.Astetic.AsteticActivity;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +33,7 @@ public class HomeFragment extends Fragment {
     private ImageButton btn5;
     private String result;
     private Intent intent1;
-    private List<String> items = Arrays.asList("반영구", "에스테틱", "왁싱", "네일", "속눈썹");
+    private FragmentPagerAdapter fragmentPagerAdapter;
 
     @Nullable
     @Override
@@ -38,6 +41,13 @@ public class HomeFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        ViewPager viewPager = (ViewPager)view.findViewById(R.id.tab_View);
+        fragmentPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+
+
+        TabLayout tabLayout = (TabLayout)view.findViewById(R.id.tab_home);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
         btn1 = view.findViewById(R.id.btn_ast);
         btn2 = view.findViewById(R.id.btn_half);
@@ -65,50 +75,5 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    public void onCreate(Bundle savedInstanceState, LayoutInflater inflater, ViewGroup container){
-        super.onCreate(savedInstanceState);
-        view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        SearchView searchView = getView().findViewById(R.id.search_button);
-        final TextView resultTextView = getView().findViewById(R.id.text_home);
-        resultTextView.setText(getResult());
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                resultTextView.setText(search(newText));
-                return true;
-            }
-        });
-    }
-
-    private String search(String query){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < items.size(); i++) {
-            String item = items.get(i);
-            if (item.toLowerCase().contains(query.toLowerCase())) {
-                sb.append(item);
-                if (i != items.size() - 1) {
-                    sb.append("\n");
-                }
-            }
-        }
-        return sb.toString();
-    }
-
-    private String getResult(){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < items.size(); i++){
-            String item = items.get(i);
-            sb.append(item);
-            if(i != items.size() -1){
-                sb.append("\n");
-            }
-        }
-        return sb.toString();
-    }
 }
--- app/src/main/java/com/example/ver1/ui/home/Shop/ShopinfoActivity.java
+++ app/src/main/java/com/example/ver1/ui/home/Shop/ShopinfoActivity.java
@@ -1,15 +1,28 @@
 package com.example.ver1.ui.home.Shop;
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.FragmentPagerAdapter;
+        import androidx.viewpager.widget.ViewPager;
 
-import com.example.ver1.R;
+        import com.example.ver1.R;
+        import com.example.ver1.ui.home.ViewPagerAdapter;
+        import com.google.android.material.tabs.TabLayout;
 
 public class ShopinfoActivity extends AppCompatActivity {
 
+    private FragmentPagerAdapter fragmentPagerAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopinfo);
+
+        ViewPager viewPager = findViewById(R.id.tab_shopvp);
+        fragmentPagerAdapter = new ShopPagerAdapter(getSupportFragmentManager());
+
+
+        TabLayout tabLayout = findViewById(R.id.tab_Shop);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
